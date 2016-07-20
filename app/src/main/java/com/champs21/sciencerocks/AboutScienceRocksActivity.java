b38caa02ea26b6af7c7787bb0e9908026205f2d9
Taufiq Ovi <ovioviovi@gmail.com>
2016-07-20 20:15:41 +0600

    1. In AboutScienceRocksActivity.java a button created at the bootom of layout by clicking it user will be rerirected to meet the anchor root activity class.

--- app/src/main/java/com/champs21/sciencerocks/AboutScienceRocksActivity.java
+++ app/src/main/java/com/champs21/sciencerocks/AboutScienceRocksActivity.java
@@ -1,16 +1,20 @@
 package com.champs21.sciencerocks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutScienceRocksActivity extends AppCompatActivity {
 
 
     private TextView txtAbout;
+    private AppCompatButton btnMta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +38,18 @@ public class AboutScienceRocksActivity extends AppCompatActivity {
 
     private void initView(){
         txtAbout = (TextView)this.findViewById(R.id.txtAbout);
+        btnMta = (AppCompatButton)this.findViewById(R.id.btnMta);
     }
 
     private void initAction(){
         txtAbout.setText(Html.fromHtml(getString(R.string.about_science_rocks)));
+        btnMta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutScienceRocksActivity.this, MeetTheAnchorsRootActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
